fa58373384cf3357e03d137ce773ebf3603d8430
stiven mendieta <stivenmendieta92@gmail.cstivenmendieta92@gmail.comm>
2026-06-05 11:54:08 -0500

    navbar.php file complete

--- views/blog.php
+++ views/blog.php
@@ -11,93 +11,9 @@
 		</div>
 		<!-- END PRELOADER -->		
 
-		<!-- START NAVBAR -->  
-		<div id="navigation" class="navbar-light bg-faded site-navigation">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-20 align-self-center">
-						<div class="site-logo">
-							<a href="index.php"><img src="../assets/img/logo.png" alt=""></a>          				
-						</div>
-					</div><!--- END Col -->
-					
-					<div class="col-60 d-flex">
-						<nav id="main-menu">
-							<ul>
-								<li class="menu-item-has-children"><a href="#">Home</a>
-									<ul>										
-										<li><a href="index.php">Home 01</a></li>
-										<li><a href="index2.php">Home 02</a></li>
-									</ul>
-								</li>
-								<li><a href="about.php">About</a></li>				  				  
-								<li class="menu-item-has-children"><a href="course.php">Course</a>
-									<ul>										
-										<li><a href="course.php">Course</a></li>
-										<li><a href="course_details.php">Course Details</a></li>
-									</ul>
-								</li>								
-								<li class="menu-item-has-children"><a href="#">Pages</a>
-									<ul>										
-										<li><a href="instructor.php">Instructor</a></li>
-										<li><a href="ins_details.php">Instructor Details</a></li>
-										<li><a href="pricing.php">Pricing Plan</a></li>
-										<li><a href="faq.php">Faq Page</a></li>			
-										<li><a href="404.php">404</a></li>				
-									</ul>
-								</li>							
-								<li class="menu-item-has-children"><a href="blog.php">Blog</a>
-									<ul>										
-										<li><a href="blog.php">Blog</a></li>
-										<li><a href="blog_single.php">Blog Details</a></li>
-									</ul>
-								</li>							  
-								<li><a href="contact.php">Contact</a></li>
-							</ul>
-						</nav>
-					</div><!--- END Col -->
-					
-					<div class="col-20 d-none d-xl-block text-end align-self-center">
-						<a href="../admin/html/index.php" class="header-btn">Sign In</a>
-						<a href="contact.php" class="btn_one">Sign Up</a>
-					</div><!--- END Col -->
-					
-					
-					<ul class="mobile_menu">						
-						<li><a href="#">Home</a>
-							<ul class="sub-menu">										
-								<li><a href="index.php">Home 01</a></li>
-								<li><a href="index2.php">Home 02</a></li>						
-							</ul>
-						</li>	
-						<li><a href="about.php">About</a></li>						
-						<li><a href="#">Course</a>
-							<ul class="sub-menu">										
-								<li><a href="course.php">Course</a></li>
-								<li><a href="course_details.php">Course Deails</a></li>									
-							</ul>
-						</li>
-						<li><a href="#">Pages</a>
-							<ul class="sub-menu">									
-								<li><a href="instructor.php">Instructor</a></li>
-								<li><a href="ins_details.php">Instructor Details</a></li>
-								<li><a href="pricing.php">Pricing Plan</a></li>
-								<li><a href="faq.php">Faq Page</a></li>			
-								<li><a href="404.php">404</a></li>							
-							</ul>
-						</li>			
-						<li><a href="blog.php">Blog</a>
-							<ul class="sub-menu">										
-								<li><a href="blog.php">Blog</a></li>
-								<li><a href="blog_single.php">Blog Details</a></li>
-							</ul>
-						</li>						
-						<li><a href="contact.php">Contact</a></li>
-					</ul>			
-				</div><!--- END ROW -->
-			</div><!--- END CONTAINER -->
-		</div> 	  
-		<!-- END NAVBAR -->		
+		<?php 
+        include'layout/navbar.php'
+		 ?>		
 
 		<!-- START SECTION TOP -->
 		<section class="section-top">
