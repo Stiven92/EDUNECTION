3dc8e9d4da50f8de7304b28ba811220ed6b50ba3
stiven mendieta <stivenmendieta92@gmail.cstivenmendieta92@gmail.comm>
2026-07-24 11:26:42 -0500

    pagina 404 traducida

--- views/404.php
+++ views/404.php
@@ -22,7 +22,7 @@
 			<div class="container">
 				<div class="col-lg-10 offset-lg-1 text-center">
 					<div class="section-top-title wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.3s" data-wow-offset="0">
-						<h1>Page Not Found</h1>
+						<h1>Página no encontrada</h1>
 						<ul>
 							<li><a href="index.php">Home</a></li>
 							<li> / 404</li>
@@ -40,10 +40,10 @@
 			  <div class="col-lg-12 col-sm-12 col-xs-12 text-center">
 					<div class="error_page">
 						<img src="../assets/img/404.svg" class="img-fluid" alt="404 error" />
-						<h2>Oops! Page not found</h2>
-						<p>Hmm. We’re having trouble finding that site. Try again later or Check your network connection.</p>
+						<h2>Oops! Página no encontrada</h2>
+						<p>Mmm. Tenemos problemas para encontrar ese sitio. Inténtalo de nuevo más tarde o comprueba tu conexión a la red..</p>
 						<div class="home_btn">
-							<a href="index.php" class="btn_one">Back to Home</a>
+							<a href="index.php" class="btn_one">volver a casa </a>
 						</div>	
 					</div>
 			  </div><!--- END COL -->				  
